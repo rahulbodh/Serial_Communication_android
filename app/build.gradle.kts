96bf51d88d3e0c33f4a1ad9bb697b6d1aff2a39f
Rahul <rahulbodh819@gmail.com>
2025-01-20 12:41:20 +0530

    Get connected device info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.serialcommunicationandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.serialcommunicationandroid"
--- app/src/main/java/com/example/serialcommunicationandroid/MainActivity.kt
+++ app/src/main/java/com/example/serialcommunicationandroid/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.serialcommunicationandroid
 
+import android.content.Context
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+
+        val deviceList = manager.getDeviceList()
+
+        for (device in deviceList) {
+            val deviceManufacturer = device.value.manufacturerName
+            val deviceClass = device.value.deviceClass
+            val deviceSubclass = device.value.deviceSubclass
+            val deviceProtocol = device.value.deviceProtocol
+            val deviceVendorId = device.value.interfaceCount
+            val deviceProductId = device.value.productId
+            Log.d("SerialDevice", "Device: $deviceManufacturer $deviceClass $deviceSubclass $deviceProtocol $deviceVendorId $deviceProductId")
+        }
+
+
     }
 }
