c80567f325b8cb884ec3a1a98733068971ebee75
Rahul <rahulbodh819@gmail.com>
2025-01-20 13:03:37 +0530

    Obtain permission to communicate with a device

--- app/src/main/java/com/example/serialcommunicationandroid/MainActivity.kt
+++ app/src/main/java/com/example/serialcommunicationandroid/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.serialcommunicationandroid
 
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private  val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+    private  var usbDevice: UsbDevice ?= null
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,11 +31,15 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+        val usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
 
-        val deviceList = manager.getDeviceList()
+
+        val deviceList = usbManager.getDeviceList()
 
         for (device in deviceList) {
+
+            usbDevice = deviceList.values.first()
+            Log.d("SerialDevice", "Found USB Device: ${usbDevice?.deviceName}")
             val deviceManufacturer = device.value.manufacturerName
             val deviceClass = device.value.deviceClass
             val deviceSubclass = device.value.deviceSubclass
@@ -35,6 +49,34 @@ class MainActivity : AppCompatActivity() {
             Log.d("SerialDevice", "Device: $deviceManufacturer $deviceClass $deviceSubclass $deviceProtocol $deviceVendorId $deviceProductId")
         }
 
+
+
+
+         val usbReceiver = object : BroadcastReceiver() {
+
+            override fun onReceive(context: Context, intent: Intent) {
+                if (ACTION_USB_PERMISSION == intent.action) {
+                    synchronized(this) {
+                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                            usbDevice?.apply {
+                                // call method to set up device communication
+                            }
+                        } else {
+                            Log.d("SerialDevice", "permission denied for device $usbDevice")
+                        }
+                    }
+                }
+            }
+        }
+
+        val permissionIntent = PendingIntent.getBroadcast(
+            this, 0,
+            Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE
+        )
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        registerReceiver(usbReceiver, filter, RECEIVER_EXPORTED)
+
+        usbManager.requestPermission(usbDevice,permissionIntent)
 
     }
 }
